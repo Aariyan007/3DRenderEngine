1b12f558cc72b774a5b0563254acf0f7150f1a5c
lYNUX <lynux@gmail.com>
2025-12-07 16:23:56 +0530

    Created Triangle and Vertex

---
+++ src/main/java/Triangle.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+
+public class Triangle {
+    double x;
+    double y;
+    double z;
+    Color color;
+
+    Triangle(double x, double y, double z, Color color){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.color = color;
+    }
+}
